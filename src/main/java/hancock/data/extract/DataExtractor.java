f2f814c4b5074b0a814df7325fbecf238768720b
Andrew Hancock <AndrewNeilHancock@gmail.com>
2017-04-28 23:41:17 -0400

    Refactored extraction code.

--- src/main/java/hancock/data/extract/DataExtractor.java
+++ src/main/java/hancock/data/extract/DataExtractor.java
@@ -1,6 +1,5 @@
 package hancock.data.extract;
 
-
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
@@ -18,55 +17,40 @@ import hancock.data.extract.jdbc.oracle.OracleMetadataExtractor;
 import hancock.data.model.Table;
 import hancock.data.model.serialization.JsonTableSerializer;
 
-public class DataExtractor {	
-	
-	private static void createTargetDirectory(String targetDir) throws Exception {
-		if(Files.isDirectory(Paths.get(targetDir))) {
-			throw new Exception("Target directory already exists.");
-		}
-		else {
-			Files.createDirectory(Paths.get(targetDir));
-		}
-	}	
+public class DataExtractor {
 
-	public static void main(String[] args) throws Exception {
-		
-		Options cliOptions = ExtractCli.getCliOptions();
-		ExtractOptions options = null;
-		try {
-			
-			options = ExtractCli.parseArguments(args, cliOptions);
-		}
-		catch (ParseException e) {
-			new HelpFormatter().printHelp("DataExtractor", cliOptions);
-			System.exit(-1);
-		}
-		
-		createTargetDirectory(options.getOutputDirectory());
-		Collection<Table> tables = JsonTableSerializer.readTableMetadata(Paths.get(options.getCataloguePath()));
-		Collection<Table> tableMetadata = OracleMetadataExtractor.extractMetadata(options.getJdbcPath(), tables);
-		JsonTableSerializer.writeTableMetadata(Paths.get(options.getOutputDirectory(), "metadata.json"), tableMetadata);		
-		
-		SparkSession spark =  SparkSession.builder()
-				.master("local")
-				.appName("SparkCSVExample")				
-				.getOrCreate();
-		
-		for(Table table : tableMetadata) {		
-			Dataset<Row> employeeTable = spark.read()
-				.format("jdbc")
-				.option("url", options.getJdbcPath())
-				.option("dbtable", table.getTableName())
-				.option("user", "hr")
-				.option("password", "test")
-				.load();
-			
-			employeeTable.write()
-				.option("header", true)
-				.csv(Paths.get(options.getOutputDirectory(), table.getTableName()).toString());
+	private static void createTargetDirectory(String targetDir) throws Exception {
+		if (Files.isDirectory(Paths.get(targetDir))) {
+			throw new Exception("Target directory already exists.");
+		} else {
+			Files.createDirectory(Paths.get(targetDir));
 		}
 	}
 
+	public static void main(String[] args) throws Exception {
 
+		Options cliOptions = ExtractCli.getCliOptions();
+		ExtractOptions options = null;
+		try {
+
+			options = ExtractCli.parseArguments(args, cliOptions);
+		} catch (ParseException e) {
+			new HelpFormatter().printHelp("DataExtractor", cliOptions);
+			System.exit(-1);
+		}
+
+		createTargetDirectory(options.getOutputDirectory());
+		Collection<Table> tables = JsonTableSerializer.readTableMetadata(Paths.get(options.getCataloguePath()));
+		Collection<Table> tableMetadata = OracleMetadataExtractor.extractMetadata(options.getJdbcPath(), tables);
+		JsonTableSerializer.writeTableMetadata(Paths.get(options.getOutputDirectory(), "metadata.json"), tableMetadata);
+
+		SparkSession spark = SparkSession.builder().master("local").appName("SparkCSVExample").getOrCreate();
+
+		TableExtractor extractor = new TableExtractor(spark, options.getJdbcPath());
+
+		final String outputDirectory = options.getOutputDirectory();
+		tableMetadata.forEach(table -> extractor.extractTable(table, Paths.get(outputDirectory, table.getTableName())));
+
+	}
 
 }
---
+++ src/main/java/hancock/data/extract/TableExtractor.java
@@ -0,0 +1,28 @@
+package hancock.data.extract;
+
+import java.nio.file.Path;
+import java.util.Properties;
+
+import org.apache.spark.sql.SparkSession;
+
+import hancock.data.model.Table;
+
+public class TableExtractor {
+	private SparkSession spark;
+	private String uri;
+
+	public TableExtractor(SparkSession spark, String uri) {
+		this.spark = spark;
+		this.uri = uri;
+
+	}
+
+	public void extractTable(Table schema, Path outputPath) {
+		spark.read()
+			.jdbc(uri, schema.getTableName(), new Properties())
+			.write()
+			.option("header", true)
+			.csv(outputPath.toString());
+		
+	}
+}
